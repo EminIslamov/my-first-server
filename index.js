661aed4f6311b453011be48376be53abdffc1e2b
emin <Magamedi98@mail.ru>
2021-07-04 17:02:54 +0300

    Исправил задачу 2.2

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-//Задача 2.2. Создай в проекте новую папку с именем pages. Внутри папки создай файл index.html с каким-нибудь HTML-кодом.
-const http = require('http')
-const path = require('path')
-const fs = require('fs')
+const http = require('http');
 
+// импортируем модуль path
+const path = require('path');
+
+// импортируем модуль fs
+const fs = require('fs');
+
+// создаем переменную сервера и принимаем в колбек нужные параметры
 const server = http.createServer((req, res) => {
-    fs.readFile(path.resolve(__dirname, 'pages', 'index.html'), (err, data) => {
-       res.writeHead(200, {
-           'Content-type': 'text/html'
-       })
-       res.write(data.toString())
-        res.end()
-    })
-})
+    // отправляем статус 200 и заголовок с указанием типа ответа
+    res.writeHead(200, {
+        "Content-type": "text/html"
+    });
 
+    // читаем содержимое файла
+    fs.readFile(path.resolve(__dirname, 'pages', 'index.html'), (err, data) => {
+        // если нет ошибок, то...
+        if(!err) {
+            // ...формируем тело ответа добавив в него содержимое html-файла
+            res.write(data.toString());
+        }
+
+        // завершаем отправку ответа внутри колбек функции, т.к. write()
+        // не может вызываться после end().
+        res.end();
+    })
+});
+
+// слушаем созданный сервер на 3000 порту...
 server.listen(3000)
