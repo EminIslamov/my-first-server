b8132aa2cd9a5865a606dcdb0521e0929eff55ac
emin <Magamedi98@mail.ru>
2021-07-04 17:00:43 +0300

    Выполнил задачу 2.2. На запрос пользователя вывел html-файл

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+//Задача 2.2. Создай в проекте новую папку с именем pages. Внутри папки создай файл index.html с каким-нибудь HTML-кодом.
 const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200)
-
-    res.write('Hello world')
-
-    res.end()
+    fs.readFile(path.resolve(__dirname, 'pages', 'index.html'), (err, data) => {
+       res.writeHead(200, {
+           'Content-type': 'text/html'
+       })
+       res.write(data.toString())
+        res.end()
+    })
 })
 
 server.listen(3000)
